74887b333e525bb0bc5efb4146665240199f7b1d
Muhammad Saqlain <haidrysaqlain92@gmail.com>
2022-03-08 18:37:16 +0500

    bcrypt defered

--- app/resturaunt/resturaunt.model.js
+++ app/resturaunt/resturaunt.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt   = require('bcrypt');
+// const bcrypt   = require('bcrypt');
 const paginate = require('../helpers/paginate');
 const Schema = mongoose.Schema;
 
